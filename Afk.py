3183a851963927e51e2aef6d023bc2e27f82e657
flixhack <felix.benjamin.mathiassen@gmail.com>
2020-02-19 11:44:58 +0100

    Update Afk.py
    update timer

--- Afk.py
+++ Afk.py
@@ -5,7 +5,7 @@ pyautogui.FAILSAFE = True
 print('Press Ctrl-C to quit.')
 try:
     while True:
-        a = randint(2, 10)
+        a = randint(100, 300)
         print(a)
         time.sleep(a)
         pyautogui.keyDown('w')
