85fd7464b81f44cfde01e1b562a48edba8272e04
Jesper Mathiassen <jesper.mathiassen@gmail.com>
2020-02-20 11:01:36 +0100

    random behavior

--- Afk.py
+++ Afk.py
@@ -1,14 +1,18 @@
 import pyautogui, sys
 import time as time
-from random import randint
+from random import randint,choice
 pyautogui.FAILSAFE = True
 print('Press Ctrl-C to quit.')
 try:
     while True:
         a = randint(100, 300)
+        c = randint(1,10)
         print(a)
         time.sleep(a)
-        pyautogui.keyDown('w')
-        pyautogui.keyUp('w')
+        for i in range(1,c):
+            k = choice(['a','s','w','d','b','x',' '])
+            pyautogui.keyDown(k)
+            time.sleep(0.05)
+            pyautogui.keyUp(k)
 except KeyboardInterrupt:
     print('\n')
