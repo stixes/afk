95797da0cb9f0622f616e2b167dcb89ac45f22bf
Jesper Mathiassen <jesper.mathiassen@gmail.com>
2020-02-23 14:23:01 +0100

    jump every 10min. ish

--- Afk.py
+++ Afk.py
@@ -1,18 +1,22 @@
 import pyautogui, sys
 import time as time
-from random import randint,choice
+from random import randint,choice,gauss
 pyautogui.FAILSAFE = True
 print('Press Ctrl-C to quit.')
 try:
     while True:
-        a = randint(100, 300)
-        c = randint(1,10)
-        print(a)
+        #a = randint(100, 300)
+        a = max(gauss(10*60,2*60),0)
+        c = randint(2,5)
+        print("Seconds to next input %d"%a)
         time.sleep(a)
+        print("Waking up..")
         for i in range(1,c):
-            k = choice(['a','s','w','d','b','x',' '])
+            k = choice([' '])
             pyautogui.keyDown(k)
-            time.sleep(0.05)
+            time.sleep(gauss(0.06,0.02))
             pyautogui.keyUp(k)
+            time.sleep(gauss(0.5,0.1))
+        print("Back to sleep.")
 except KeyboardInterrupt:
     print('\n')
